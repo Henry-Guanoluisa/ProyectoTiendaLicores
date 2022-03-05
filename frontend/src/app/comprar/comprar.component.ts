fcb0301cfb30cde9bf58f4472854d1e7504b62f8
Henry-Guanoluisa <92236114+Henry-Guanoluisa@users.noreply.github.com>
2022-03-05 17:15:35 -0500

    front

--- frontend/src/app/comprar/comprar.component.ts
+++ frontend/src/app/comprar/comprar.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {LicorService} from "../services/licor.service";
-import{ActivatedRoute} from "@angular/router";
-
+import { LicorService } from "../services/licor.service";
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-comprar',
   templateUrl: './comprar.component.html',
   styleUrls: ['./comprar.component.css']
 })
 export class ComprarComponent implements OnInit {
-
+  allProducts = []
   constructor(private licorService: LicorService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe((query)=>{
-      if(query["licor"]){
-        console.log(query["licor"])
-      }
+    this.licorService.getProducts().subscribe((data) => {
+      this.allProducts = data.productos
+      console.log(this.allProducts)
 
     })
-    // this.licorService.getProducts().subscribe((data)=>{
-    //   console.log(data)
-    // })
+    this.activatedRoute.queryParams.subscribe(query => {
+      const buscador  = query["licor"]
+      if (buscador) {
+        console.log("query ", buscador)
+        this.allProducts.forEach((product) => {
+          if (product['titulo'] == query["licor"]) {
+            console.log("product ", product)
+            this.allProducts.push(product)
+          }
+        })
+      }
+    })
+
   }
 
 }
